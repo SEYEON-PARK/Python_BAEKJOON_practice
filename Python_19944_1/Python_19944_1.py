31313818862fa2a12479f6a74d1879b019fae152
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-12-15 07:02:35 +0900

    Python언어 BAEKJOON 19944번 문제 풀기!(1)

--- Python_19944_1/Python_19944_1.py
+++ Python_19944_1/Python_19944_1.py
@@ -10,3 +10,11 @@ N과 M이 주어졌을 때, M학년이 뉴비인지 올드비인지 TLE인지 �
 M학년이 뉴비라면 NEWBIE!를, 올드비라면 OLDBIE!를 TLE이라면 TLE!을 출력합니다.
 '''
 
+N, M=map(int, input().split())
+
+if(M==1 or M==2):
+    print("NEWBIE!")
+elif(M<=N):
+    print("OLDBIE!")
+else:
+    print("TLE!")
