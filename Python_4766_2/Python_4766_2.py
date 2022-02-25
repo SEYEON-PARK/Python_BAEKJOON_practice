5770bcead1c414e14dcd1d2645e1a9cd0900a87b
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-02-25 19:09:24 +0900

    Python언어 BAEKJOON 4766번 문제 풀기!(2)

--- Python_4766_2/Python_4766_2.py
+++ Python_4766_2/Python_4766_2.py
@@ -8,3 +8,11 @@
 
 입력으로 주어진 각 온도와 이전 온도와의 차이를 출력한다. 첫 번째 측정할 온도는 이전 온도가 없으니 출력할 필요가 없다. 차이는 항상 소수점 둘째자리까지 출력한다.
 '''
+
+first=float(input())
+second=float(input())
+
+while(second!=999):
+    print("%0.2f" %(second-first))
+    first=second
+    second=float(input())
