1302ed4c39167dbcf48c73d604ecc7ea8e0b3601
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-03-15 00:40:09 +0900

    Python언어 BAEKJOON 15873번 문제 풀기!(3)

--- Python_15873_3/Python_15873_3.py
+++ Python_15873_3/Python_15873_3.py
@@ -5,3 +5,22 @@
 
 첫 번째 줄에 A+B의 값을 출력한다.
 '''
+
+n=int(input())
+a=n
+b=n
+
+if len(str(n)) >= 3:
+    if(n//10==10):
+        a%=100
+        b//=10
+        print(a+b)
+    else:
+        a%=100
+        b//=100
+        print(a+b)
+    
+else:
+    a%=10
+    b//=10
+    print(a+b)
