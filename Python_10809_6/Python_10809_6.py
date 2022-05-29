1408b2cee0054f7b0bf7053840d46238a00bf3d8
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-05-30 01:58:04 +0900

    Python언어 BAEKJOON 10809번 문제 풀기!(6)

--- Python_10809_6/Python_10809_6.py
+++ Python_10809_6/Python_10809_6.py
@@ -15,5 +15,5 @@ S=input() # 사용자로부터 문자열 입력받기
 for i in alphabet_list: # 문자열 alphabet_list의 맨 앞 글자부터 하나씩 i에 대입하며 반복
     if i in S: # 만약, i가 문자열 S 안에 있다면
         print(S.index(i), end=" ") # 문자열 S 안에 있는 i의 위치(인덱스 번호) 출력하기
-    else:
-        print(-1, end=' ')
+    else: # i가 문자열 S 안에 없다면
+        print(-1, end=' ') # -1 출력하기
