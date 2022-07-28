a4e6f1f15bfbdf2006146a9e0a7624df13a85684
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-07-29 01:14:48 +0900

    Python언어 BAEKJOON 1547번 문제 풀기!(1)

--- Python_1547_1/Python_1547_1.py
+++ Python_1547_1/Python_1547_1.py
@@ -17,10 +17,10 @@ M=int(input()) # 사용자로부터 정수 입력받기
 ball_cup=1 # ball_cup에 1 대입하기
 
 for i in range(M): # i에 0부터 M이 되기 전까지 1씩 증가한 값을 대입하며 반복
-    A, B=map(int, input().split())
-    if(A==ball_cup):
-        ball_cup=B
-    elif(B==ball_cup):
+    A, B=map(int, input().split()) # 사용자로부터 두 개의 정수 입력받기
+    if(A==ball_cup): # 만약, A의 값이 ball_cup의 값과 같다면
+        ball_cup=B # ball_cup에 B의 값 대입하기
+    elif(B==ball_cup): # 
         ball_cup=A
 
 print(ball_cup)
