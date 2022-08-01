80540da1c5cebd9fc4927d492c3e2839911bb385
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-08-02 02:04:14 +0900

    Python언어 BAEKJOON 1547번 문제 풀기!(3)

--- Python_1547_3/Python_1547_3.py
+++ Python_1547_3/Python_1547_3.py
@@ -13,11 +13,11 @@ X와 Y의 값은 3보다 작거나 같고, X와 Y가 같을 수도 있다.
 첫째 줄에 공이 들어있는 컵의 번호를 출력한다. 공이 사라져서 컵 밑에 없는 경우에는 -1을 출력한다.
 '''
 
-M=int(input())
-ball_cup=1
+M=int(input()) # 사용자로부터 정수 입력받기
+ball_cup=1 # ball_cup에 1 대입하기
 
-for i in range(M,0,-1):
-    A, B=map(int, input().split())
+for i in range(M,0,-1): # M부터 0이 되기 전까지 1씩 감소시킨 값을 i에 대입하며 반복
+    A, B=map(int, input().split()) # 사용자로부터 두 개의 정수 입력받기
     if(A==ball_cup):
         ball_cup=B
     elif(B==ball_cup):
