49afba8b3b973d737fb23b378b9f6c48bd5af523
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-11-24 23:56:49 +0900

    Python언어 BAEKJOON 11720번 문제 풀기!(1)

--- Python_11720_1/Python_11720_1.py
+++ Python_11720_1/Python_11720_1.py
@@ -10,7 +10,7 @@ N=int(input()) # 사용자로부터 정수 입력받기
 s=input() # 숫자들을 문자열로 입력받기
 sum=0 # 변수 sum 선언 및 초기화
 
-for i in s:
-    sum+=int(i)
+for i in s: # 문자열 s에 담긴 내용 하나씩 i에 넣어가며 반복 
+    sum+=int(i) # sum에 sum+int(i)의 값 대입하기
 
-print(sum)
+print(sum) # 결론 출력하기
