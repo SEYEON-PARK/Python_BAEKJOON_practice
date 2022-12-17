305571026b703a3c329dea1227d797e6966bf5de
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-12-18 00:42:28 +0900

    Python언어 BAEKJOON 10807번 문제 풀기!(1)

--- Python_10807_1/Python_10807_1.py
+++ Python_10807_1/Python_10807_1.py
@@ -6,3 +6,14 @@
 
 첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
 '''
+
+N=int(input())
+num=list(map(int, input().split()))
+v=int(input())
+count=0
+
+for i in num:
+    if(v==i):
+        count+=1
+    
+print(count)
