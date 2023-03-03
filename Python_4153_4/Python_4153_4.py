d444ed0c81c026045ae1142d4a7cabac4eec4f9e
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-03-04 00:31:55 +0900

    Python언어 BAEKJOON 4153번 문제 풀기!(4)

--- Python_4153_4/Python_4153_4.py
+++ Python_4153_4/Python_4153_4.py
@@ -5,3 +5,17 @@
 
 각 입력에 대해 직각 삼각형이 맞다면 "right", 아니라면 "wrong"을 출력한다.
 """
+
+def square(a):
+    return a*a;
+
+while(True):
+    line=list(map(int, input().split()))
+    line.sort()
+    
+    if(line[0]==0 and line[1]==0 and line[2]==0):
+        break;
+    elif(square(line[2])==square(line[0])+square(line[1])):
+        print("right");
+    else:
+        print("wrong");
