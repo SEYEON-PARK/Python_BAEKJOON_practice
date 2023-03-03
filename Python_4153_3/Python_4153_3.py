a30380e4721f105de80840c66af46d9f70160d1b
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-03-04 00:18:40 +0900

    Python언어 BAEKJOON 4153번 문제 풀기!(3)

--- Python_4153_3/Python_4153_3.py
+++ Python_4153_3/Python_4153_3.py
@@ -8,11 +8,11 @@
 
 while(True): # 무한 반복
     line=list(map(int, input().split())) # 사용자로부터 세 개의 정수를 입력받아 리스트 line에 저장하기
-    line=sorted(line) # 리
+    line=sorted(line) # 리스트 line을 오름차순 정리하여 line에 저장하기
     
-    if(line[0]==0 and line[1]==0 and line[2]==0):
-        break;
-    elif(line[2]*line[2]==line[0]*line[0]+line[1]*line[1]):
-        print("right");
-    else:
-        print("wrong");
+    if(line[0]==0 and line[1]==0 and line[2]==0): # 만약, line[0]이 0이고, line[1]도 0이고, line[2]도 0이라면
+        break; # 반복문 빠져 나가기
+    elif(line[2]*line[2]==line[0]*line[0]+line[1]*line[1]): # 만약, line[0]이 0이 아니거나, line[1]이 0이 아니거나, line[2]이 0이 아니고, line[2]의 제곱이 (line[0]의 제곱 + line[1]의 제곱)과 같다면
+        print("right"); # "right" 출력하기
+    else: # line[0]이 0이 아니거나, line[1]이 0이 아니거나, line[2]이 0이 아니고, line[2]의 제곱이 (line[0]의 제곱 + line[1]의 제곱)과 같지 않다면
+        print("wrong"); # "wrong" 출력하기
