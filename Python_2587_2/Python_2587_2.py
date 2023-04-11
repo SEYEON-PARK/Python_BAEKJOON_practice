28d7567b06b22084f0ca1615e2b835f5924bb841
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-04-12 02:26:17 +0900

    Python언어 BAEKJOON 2587번 문제 풀기!(2)

--- Python_2587_2/Python_2587_2.py
+++ Python_2587_2/Python_2587_2.py
@@ -9,3 +9,17 @@
 
 첫째 줄에는 평균을 출력하고, 둘째 줄에는 중앙값을 출력한다. 평균과 중앙값은 모두 자연수이다.
 '''
+
+num=[0, 0, 0, 0, 0]
+
+for i in range(5):
+    num[i]=int(input())
+
+num.sort()
+sum=0
+
+for i in num:
+    sum+=i
+    
+print(int(sum/5))
+print(num[2])
