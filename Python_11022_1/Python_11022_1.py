e81bbf324a6e36eea211985103deadeda00ba5da
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-06-19 00:44:25 +0900

    Python언어 BAEKJOON 11022번 문제 풀기!(1)

--- Python_11022_1/Python_11022_1.py
+++ Python_11022_1/Python_11022_1.py
@@ -7,8 +7,8 @@
 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
 '''
 
-N=int(input())
+N=int(input()) # 사용자로부터 정수 입력받기
 
-for i in range(N):
+for i in range(N): # i에 0부터 N이 되기 전까지 1씩 증가시킨 값을 대입하며 반복
     A, B=map(int, input().split())
     print("Case #"+str(i+1)+": "+str(A)+" + "+str(B)+" = "+str(A+B))
