b310b8d130dbeeb7f56f3d7b232b37dfcafe5c6c
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-08-19 00:08:13 +0900

    Python언어 BAEKJOON 10430번 문제 풀기!(1)

--- Python_10430_1/Python_10430_1.py
+++ Python_10430_1/Python_10430_1.py
@@ -8,7 +8,7 @@
 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
 """
 
-A, B, C=map(int, input().split())
+A, B, C=map(int, input().split()) # 사용자로부터 세 개의 정수 입력받기
 
 print((A+B)%C)
 print(((A%C) + (B%C))%C)
