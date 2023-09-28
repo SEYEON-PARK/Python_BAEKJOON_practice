7cd8d37a5734da2507477552e7d9ed8fd5f5f82e
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-09-29 04:11:27 +0900

    Python언어 BAEKJOON 10869번 문제 풀기!(1)

--- Python_10869_1/Python_10869_1.py
+++ Python_10869_1/Python_10869_1.py
@@ -6,7 +6,7 @@
 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
 '''
 
-A, B=map(int, input().split())
+A, B=map(int, input().split()) # 사용자로부터 두 개의 정수 입력받기
 print(A+B)
 print(A-B)
 print(A*B)
