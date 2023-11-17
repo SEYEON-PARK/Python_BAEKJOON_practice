86f76da027c62cf8a3e4b1b871ae60ed9195f5cf
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-11-17 17:06:17 +0900

    Python언어 BAEKJOON 27433번 문제 풀기!(1)

--- Python_27433_1/Python_27433_1.py
+++ Python_27433_1/Python_27433_1.py
@@ -9,7 +9,7 @@
 N=int(input()) # 사용자로부터 정수 입력받기
 result=1 # 결과를 저장할 변수 result 선언 및 초기화
 
-for i in range(2, N+1):
-    result*=i
+for i in range(2, N+1): # i에 2부터 N+1이 되기 전까지 1씩 증가시킨 값을 대입하며 반복
+    result*=i # result에 result*i의 값 대입하기!
     
 print(result)
