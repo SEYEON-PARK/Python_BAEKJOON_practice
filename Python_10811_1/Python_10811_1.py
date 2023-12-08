530aafff13bd3c3e09e197180e9ce2a07d046c70
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-12-08 21:27:39 +0900

    Python언어 BAEKJOON 10811번 문제 풀기!(1)

--- Python_10811_1/Python_10811_1.py
+++ Python_10811_1/Python_10811_1.py
@@ -10,3 +10,16 @@
 
 모든 순서를 바꾼 다음에, 가장 왼쪽에 있는 바구니부터 바구니에 적혀있는 순서를 공백으로 구분해 출력한다.
 '''
+
+N, M = map(int, input().split())
+bag = list()
+
+for i in range(N):
+    bag.append(i+1)
+
+for i in range(M):
+    a, b = map(int, input().split())
+    bag[a-1:b] = bag[a-1:b][::-1]
+    
+for i in range(N):
+    print(bag[i], end=" ")
