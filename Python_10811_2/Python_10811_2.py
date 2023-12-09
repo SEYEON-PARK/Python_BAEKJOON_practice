ecaef8b5adbcb94982cc3d65d599b2461a64b9dc
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-12-10 02:09:54 +0900

    Python언어 BAEKJOON 10811번 문제 풀기!(2)

--- Python_10811_2/Python_10811_2.py
+++ Python_10811_2/Python_10811_2.py
@@ -1,2 +1,15 @@
 '''
 '''
+
+N, M = map(int, input().split())
+bag = list()
+
+for i in range(N):
+    bag.append(i+1)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    bag[a-1:b] = bag[a-1:b][::-1]
+    
+for i in range(N):
+    print(bag[i], end=" ")
