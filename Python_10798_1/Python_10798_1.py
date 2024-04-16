e852df64b2b118f437e4978bc018d1981642e5b4
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-04-17 00:39:55 +0900

    Python언어 BAEKJOON 10798번 문제 풀기!(1)

--- Python_10798_1/Python_10798_1.py
+++ Python_10798_1/Python_10798_1.py
@@ -24,3 +24,20 @@ Aa0aPAf985Bz1EhCz2W3D1gkD6x
 
 영석이가 세로로 읽은 순서대로 글자들을 출력한다. 이때, 글자들을 공백 없이 연속해서 출력한다. 
 '''
+
+stringList = list()
+
+for _ in range(5):
+    s = input()
+    stringList.append(s)
+
+result = ""
+
+for i in range(15):
+    for j in range(5):
+        try:
+            result += stringList[j][i]
+        except IndexError:
+            continue
+            
+print(result)
