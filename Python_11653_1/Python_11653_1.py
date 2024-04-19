75617e835566939b6b4021bc9d8aec02b00c23d7
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-04-19 17:34:17 +0900

    Python언어 BAEKJOON 11653번 문제 풀기!(1)

--- Python_11653_1/Python_11653_1.py
+++ Python_11653_1/Python_11653_1.py
@@ -5,3 +5,13 @@
 
 N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
 '''
+
+N = int(input())
+
+while(N != 1):
+    for i in range(2, N+1):
+        if(N % i == 0):
+            print(i)
+            N //= i
+            break
+            
