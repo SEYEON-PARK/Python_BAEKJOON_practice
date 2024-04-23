814fe96fd9b9a9b7dec892281dd5717f7026044f
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-04-23 14:42:13 +0900

    Python언어 BAEKJOON 2566번 문제 풀기!(1)

--- Python_2566_1/Python_2566_1.py
+++ Python_2566_1/Python_2566_1.py
@@ -17,3 +17,22 @@
 
 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 위치한 행 번호와 열 번호를 빈칸을 사이에 두고 차례로 출력한다. 최댓값이 두 개 이상인 경우 그 중 한 곳의 위치를 출력한다.
 '''
+
+numList = [0]*9
+
+for i in range(9):
+    numList[i] = list(map(int, input().split()))
+        
+big = numList[0][0]
+indexI = 0;
+indexJ = 0;
+
+for i in range(9):
+    for j in range(9):
+        if(numList[i][j] > big):
+            big = numList[i][j]
+            indexI = i;
+            indexJ = j;
+            
+print(big)
+print(indexI+1, indexJ+1)
