90809ebeacddba7c176e5b6b681fb47ecb0495c5
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-05-14 17:30:22 +0900

    Python언어 BAEKJOON 10828번 문제 풀기!(1)

--- Python_10828_1/Python_10828_1.py
+++ Python_10828_1/Python_10828_1.py
@@ -12,3 +12,30 @@
 
 출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
 '''
+
+import sys;
+
+N = int(sys.stdin.readline().rstrip())
+stack = list()
+
+for i in range(N):
+    command = sys.stdin.readline().rstrip().split()
+    if(command[0]=='push'):
+        stack.append(command[1])
+    elif(command[0]=='pop'):
+        if(len(stack)!=0):
+            print(stack.pop())
+        else:
+            print('-1')
+    elif(command[0]=='size'):
+        print(len(stack))
+    elif(command[0]=='empty'):
+        if(len(stack)!=0):
+            print('0')
+        else:
+            print('1')
+    else:
+        if(len(stack)!=0):
+            print(stack[-1])
+        else:
+            print('-1')
