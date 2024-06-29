ea53e17eb3cb43b8448c0bedf55727e41b138f8e
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-06-30 00:34:59 +0900

    Python언어 BAEKJOON 25304번 문제 풀기!(1)

--- Python_25304_1/Python_25304_1.py
+++ Python_25304_1/Python_25304_1.py
@@ -12,3 +12,18 @@
 
 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하면 Yes를 출력한다. 일치하지 않는다면 No를 출력한다.
 '''
+
+import sys
+
+totalPrice = int(sys.stdin.readline().rstrip())
+num = int(sys.stdin.readline().rstrip())
+sum = 0
+
+for i in range(num):
+    price, count = map(int, sys.stdin.readline().rstrip().split())
+    sum += price * count
+    
+if(totalPrice == sum):
+    print("Yes")
+else:
+    print("No")
