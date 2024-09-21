f9b604bc66b2281b5d45d24b54d3c821283f22b5
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-09-21 22:52:47 +0900

    Python언어 BAEKJOON 11651번 문제 풀기!(1)

--- Python_11651_1/Python_11651_1.py
+++ Python_11651_1/Python_11651_1.py
@@ -7,13 +7,13 @@
 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
 '''
 
-N = int(input())
-li = list()
-for i in range(N):
-    x, y = map(int, input().split())
-    li.append([x, y])
+N = int(input()) # 사용자로부터 정수 입력받기
+li = list() # 리스트 li 생성
+for i in range(N): # i에 0부터 N이 되기 전까지 1씩 증가시킨 값을 대입하며 반복
+    x, y = map(int, input().split()) # 사용자로부터 두 개의 정수(x좌표, y좌표) 입력받기
+    li.append([x, y]) # li에 [x, y] 추가하기
 
-li.sort(key = lambda a : (a[1], a[0]))
+li.sort(key = lambda a : (a[1], a[0])) # li를 y좌표 기준 오름차순 정렬, 같으면 x좌표 기준 오름차순 정렬
 
-for a in li:
-    print(a[0], a[1])
+for a in li: # li에 있는 요소를 하나씩 a에 대입하며 반복
+    print(a[0], a[1]) # 정렬한 결과 출력하기
